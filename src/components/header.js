26deb6e2e7f70fc424db7c8f69268d93ddb53731
malan88 <malan88@standingwater.io>
2020-06-06 08:55:34 -0400

    Fix up the header, eliminate picture

--- src/components/header.js
+++ src/components/header.js
@@ -5,34 +5,34 @@ import heron from "../images/heron.svg"
 import styled from "styled-components"
 
 const Title = styled(Link)`
-    font-family: Space Mono;
+    font-family: Hack;
+    color: black;
 
-    color: white;
     text-decoration: none;
     font-size: 3rem;
 
-    text-align: center;
     margin: 0 auto;
-    text-shadow: 4px 4px 5px #000000;
 `
 const HeadContainer = styled.div`
     width: 50%;
     margin: 0 auto;
-    padding: 10rem;
+    text-align: center;
 `
 const Header = ({ siteTitle }) => (
     <HeadContainer>
         <Title to="/">
             {siteTitle}
         </Title>
-        <img
-            src={heron}
-            alt="Heron Logo"
-            style={{
-                verticalAlign: "middle",
-                minWidth: "75px",
-            }}
-        />
+        <Title to="/">
+            <img
+                src={heron}
+                alt="Heron Logo"
+                style={{
+                    verticalAlign: "middle",
+                    minWidth: "75px",
+                }}
+            />
+        </Title>
     </HeadContainer>
 )
 
