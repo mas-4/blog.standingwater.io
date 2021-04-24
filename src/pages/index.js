00caef65a51134c0477ea3bf51980e5eb8eefdd7
malan88 <m@stdwtr.io>
2021-04-24 16:39:40 -0400

    Extend length of excerpt

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,7 +69,7 @@ export const query = graphql`
             title
             date(formatString: "DD MMMM, YYYY")
           }
-          excerpt
+          excerpt(pruneLength: 380)
         }
       }
     }
